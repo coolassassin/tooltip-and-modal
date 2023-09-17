2511a692e8a7c823e8787b04dbecfed7c7d3c5ec
mikrise2 <mikrise2@mail.ru>
2023-09-17 21:47:56 +0200

    done

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 import './style.css';
 
-console.log('Hello world');
+const modalButton = document.getElementById('modal-button');
+const modal = document.getElementById('modal');
+const modalCloseButton = document.getElementById('modal-close-button');
+const toolTipButton = document.getElementById('tooltip-button');
+const toolTipText = document.getElementById('tooltip');
+
+modalButton.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+modalCloseButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+toolTipButton.addEventListener('click', () => {
+    if (toolTipText.style.visibility === 'visible') {
+        toolTipText.style.visibility = 'hidden'
+    } else{
+        toolTipText.style.visibility = 'visible'
+    }
+})
+
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
