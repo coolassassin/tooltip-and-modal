3b6db684a177e8148c8bb95f0b694cf2b8d88bbf
AliceGl <alice2003b@gmail.com>
2023-09-19 12:18:00 +0200

    done

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 import './style.css';
 
-console.log('Hello world');
+function tooltip_enable(event) {
+    var tooltip = document.createElement("span")
+    tooltip.innerHTML = "Hello World!";
+    tooltip.setAttribute("class", "tooltip");
+    event.srcElement.appendChild(tooltip);
+}
+function tooltip_disable(event) {
+    event.srcElement.removeChild(event.srcElement.lastChild);
+}
+
+document.getElementById("on_hover").addEventListener("mouseover", tooltip_enable);
+document.getElementById("on_hover").addEventListener("mouseout", tooltip_disable);
+
+document.getElementById("on_click").addEventListener("focus", tooltip_enable);
+document.getElementById("on_click").addEventListener("blur", tooltip_disable);
