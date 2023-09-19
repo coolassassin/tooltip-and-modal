c6776bd64251853c1de5fde605e735e676e7ee0a
mikrise2 <mikrise2@mail.ru>
2023-09-20 01:06:07 +0200

    fixed

--- main.js
+++ main.js
@@ -1,29 +1,47 @@
-import './style.css';
+import './style.css'
 
-const modalButton = document.getElementById('modal-button');
-const modal = document.getElementById('modal');
-const modalCloseButton = document.getElementById('modal-close-button');
-const toolTipButton = document.getElementById('tooltip-button');
-const toolTipText = document.getElementById('tooltip');
+const modalButton = document.getElementById('modal-button')
+const modal = document.getElementById('modal')
+const modalCloseButton = document.getElementById('modal-close-button')
+const toolTipButtonHover = document.getElementById('tooltip-hover-button')
+const toolTipButtonClick = document.getElementById('tooltip-button')
 
 modalButton.addEventListener('click', () => {
-    modal.style.display = 'block';
+    modal.style.display = 'block'
 });
 
 modalCloseButton.addEventListener('click', () => {
-    modal.style.display = 'none';
+    modal.style.display = 'none'
 });
 
-toolTipButton.addEventListener('click', () => {
-    if (toolTipText.style.visibility === 'visible') {
-        toolTipText.style.visibility = 'hidden'
-    } else{
-        toolTipText.style.visibility = 'visible'
+function addToolTip(event) {
+    let tooltip = document.createElement("span")
+    tooltip.innerText = event.target.getAttribute("data-tooltip")
+    tooltip.setAttribute("class", "tooltip-text")
+    event.target.appendChild(tooltip)
+}
+
+function removeToolTip(event) {
+    let btn = event.target;
+    btn.removeChild(btn.lastChild)
+}
+
+function addOrRemoveTooltip(event) {
+    let checker = event.target.querySelector(".tooltip-text")
+    if (checker) {
+        removeToolTip(event)
+    } else {
+        addToolTip(event)
     }
-})
+}
 
 window.addEventListener('click', (e) => {
     if (e.target === modal) {
-        modal.style.display = 'none';
+        modal.style.display = 'none'
     }
-});
+})
+
+toolTipButtonClick.addEventListener("click", addOrRemoveTooltip)
+
+toolTipButtonHover.addEventListener("mouseover", addToolTip)
+toolTipButtonHover.addEventListener("mouseout", removeToolTip)
