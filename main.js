dae141eedef0afcd2b0b3e7153ae60a91f33e2fc
AliceGl <alice2003b@gmail.com>
2023-09-21 11:53:01 +0200

    fixed

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ import './style.css';
 function tooltip_enable(event) {
     var tooltip = document.createElement("span")
     tooltip.innerHTML = "Hello World!";
-    tooltip.setAttribute("class", "tooltip");
-    event.srcElement.appendChild(tooltip);
+    tooltip.className = "tooltip";
+    this.appendChild(tooltip);
 }
 function tooltip_disable(event) {
-    event.srcElement.removeChild(event.srcElement.lastChild);
+    this.removeChild(this.lastChild);
+}
+
+const modal = document.getElementById("modal");
+
+function open_modal(event) {
+    modal.style.display = "block";
+}
+function close_modal(event) {
+    modal.style.display = "none";
 }
 
 document.getElementById("on_hover").addEventListener("mouseover", tooltip_enable);
@@ -15,3 +24,6 @@ document.getElementById("on_hover").addEventListener("mouseout", tooltip_disable
 
 document.getElementById("on_click").addEventListener("focus", tooltip_enable);
 document.getElementById("on_click").addEventListener("blur", tooltip_disable);
+
+document.getElementById("show_modal").addEventListener("click", open_modal);
+document.getElementById("modal_ok").addEventListener("click", close_modal);
