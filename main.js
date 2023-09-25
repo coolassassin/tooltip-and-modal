9d21ad9605360743ea17ad83ec438992adaeb78d
Daoortor <david.dshvsk@gmail.com>
2023-09-25 13:48:56 +0200

    hw-1

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 import './style.css';
 
-console.log('Hello world');
+let btn2 = document.getElementById("tooltip-2");
+let tooltip2 = document.getElementById("tooltip-content-2");
+let btn3 = document.getElementById("modal-button");
+let modal = document.getElementById("modal-window");
+
+function updateTooltip2(event) {
+  if (event.target == btn2 && tooltip2.style.visibility != "visible") {
+    tooltip2.style.visibility = "visible";
+  } else {
+    tooltip2.style.visibility = "hidden";
+  }
+  if (event.target == btn3 && modal.style.display == "none") {
+  	modal.style.display = "block";
+  } else if (event.target != modal) {
+  	modal.style.display = "none";
+  }
+}
+
+
+document.addEventListener("click", updateTooltip2);
+
