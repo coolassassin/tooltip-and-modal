1f3af9acd9ed1b1e713aeba3df4e0ca19abc5a81
Yurij Zaytsev <77232884+Hello-zoka@users.noreply.github.com>
2023-09-25 03:51:03 +0200

    solved

--- main.js
+++ main.js
@@ -1,3 +1,61 @@
 import './style.css';
 
-console.log('Hello world');
+
+const modal = document.getElementById("modal");
+
+function open_modal(event) {
+    this.blur()
+    this.classList.toggle("clicked");
+    modal.style.display = "block";
+}
+function close_modal(event) {
+    this.blur()
+    this.classList.toggle("clicked");
+    modal.style.display = "none";
+
+}
+
+document.getElementById("modal_button").addEventListener("click", open_modal)
+document.getElementById("close_modal").addEventListener("click", close_modal)
+document.getElementById("ok_modal").addEventListener("click", close_modal)
+document.getElementById("cancel_modal").addEventListener("click", close_modal)
+
+
+
+function add_tooltip(event) {
+    this.blur()
+    this.classList.toggle("clicked");
+    const tooltip = document.createElement("span")
+    tooltip.innerHTML = "Hello World!";
+    tooltip.className = "tooltip";
+    this.appendChild(tooltip);
+}
+
+function remove_tooltip(event) {
+    this.blur()
+    this.classList.toggle("clicked");
+    this.removeChild(this.lastChild)
+}
+
+let counter = 0;
+function handle_tooltip(event) {
+    this.blur()
+    this.classList.toggle("clicked");
+    // I didn't understand how to call methods from above....(
+    if (counter++ % 2 === 0) {
+        const tooltip = document.createElement("span")
+        tooltip.innerHTML = "Hello World!";
+        tooltip.className = "tooltip";
+        this.appendChild(tooltip);
+    } else {
+        this.removeChild(this.lastChild)
+    }
+}
+
+
+
+document.getElementById("on_hover").addEventListener("mouseover", add_tooltip)
+document.getElementById("on_hover").addEventListener("mouseout", remove_tooltip)
+
+document.getElementById("on_click").addEventListener("click", handle_tooltip)
+// document.getElementById("on_click").addEventListener("", remove_tooltip)
