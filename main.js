7a74f2e5d843a5234c64a6e4a36ec3c622dbbcca
Mark Ipatov <markthehopeful@gmail.com>
2023-09-27 15:41:46 +0200

    Buttons and tooltips

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 import './style.css';
 
+const button_on_hover = document.getElementById("on_hover")
+const button_on_click = document.getElementById("on_click")
+const button_on_click_modal = document.getElementById("on_click_modal")
+
 console.log('Hello world');
