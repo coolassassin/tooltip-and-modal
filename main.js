0ac79cd27476bfa9dba966e5211cc8647f08f661
Daoortor <david.dshvsk@gmail.com>
2023-09-28 13:04:15 +0200

    hw-1-fixes

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import './style.css';
 
+let btn1 = document.getElementById("tooltip-1");
+let tooltip1 = document.getElementById("tooltip-content-1");
 let btn2 = document.getElementById("tooltip-2");
 let tooltip2 = document.getElementById("tooltip-content-2");
 let btn3 = document.getElementById("modal-button");
@@ -7,22 +9,28 @@ let modal = document.getElementById("modal-window");
 let shadow = document.getElementById("shadow");
 let modalCloseButton = document.getElementById("modal-close-button");
 
-function updateTooltip2(event) {
-	console.log(event.target);
-  if (event.target == btn2 && tooltip2.style.display != "block") {
-    tooltip2.style.display = "block";
-  } else {
-    tooltip2.style.display = "none";
-  }
-  if (event.target == btn3 && modal.style.display != "block") {
-  	modal.style.display = "block";
-  	shadow.style.display = "block";
-  } else if (!modal.contains(event.target) || event.target == modalCloseButton) {
-  	modal.style.display = "none";
-  	shadow.style.display = "none";
-  }
+function updateOnClick(event) {
+	if (btn2.contains(event.target) && tooltip2.style.display !== "block") {
+		tooltip2.style.display = "block";
+	} else {
+		tooltip2.style.display = "none";
+	}
+	if (btn3.contains(event.target) && modal.style.display !== "block") {
+		shadow.style.display = "block";
+	} else if (!modal.contains(event.target) || modalCloseButton.contains(event.target)) {
+		shadow.style.display = "none";
+	}
+}
+
+function updateOnHover(event) {
+	if (btn1.contains(event.target)) {
+		tooltip1.style.display = "block";
+	} else {
+		tooltip1.style.display = "none";
+	}
 }
 
 
-document.addEventListener("click", updateTooltip2);
+document.addEventListener("click", updateOnClick);
+document.addEventListener("mouseover", updateOnHover);
 
