cfb57488cb09a552417a9c5bd38f08b5a1c8f08a
Mark Ipatov <markthehopeful@gmail.com>
2023-09-28 16:03:22 +0200

    Tooltip on hover and Tooltip on click!

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 import './style.css';
 
-const button_on_hover = document.getElementById("on_hover")
-const button_on_click = document.getElementById("on_click")
-const button_on_click_modal = document.getElementById("on_click_modal")
+const button_on_hover = document.getElementById("on_hover");
+const tooltip_on_hover = document.getElementById("on_hover_tooltip");
+const button_on_click = document.getElementById("on_click");
+const tooltip_on_click = document.getElementById("on_click_tooltip");
+const button_on_click_modal = document.getElementById("on_click_modal");
+
+button_on_hover.addEventListener("mouseover", () => {
+    tooltip_on_hover.style["visibility"] = "visible";
+})
+button_on_hover.addEventListener("mouseout", () => {
+    tooltip_on_hover.style["visibility"] = "hidden";
+})
+
+button_on_click.addEventListener("click", () => {
+    tooltip_on_click.style["visibility"] = (tooltip_on_click.style["visibility"] === "visible") ? "hidden" : "visible";
+})
 
 console.log('Hello world');
