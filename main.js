ee48a1e96b35f9acf945d89efc6e549e679459c2
GlebSolovev <lionyoung@mail.ru>
2023-09-29 05:05:20 +0200

    Create three buttons

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 import './style.css';
 
 console.log('Hello world');
+
+function appendButton(to, buttonText) {
+    let button = document.createElement('button');
+    button.textContent = buttonText;
+    to.appendChild(button);
+    return button;
+}
+
+let buttonsDiv = document.createElement('div');
+buttonsDiv.className = 'buttons';
+
+let hooverButton = appendButton(buttonsDiv, 'Tooltip on hoover');
+let clickButton = appendButton(buttonsDiv, 'Tooltip on click');
+let modalButton = appendButton(buttonsDiv, 'Modal on click');
+
+document.getElementById('app').append(buttonsDiv);
+
+hooverButton.addEventListener('mouseover', function (event) {
+    console.log(this);
+});
+hooverButton.addEventListener('mouseout', function (event) {
+    console.log(this);
+});
+
+clickButton.addEventListener('click', function (event) {
+    console.log(this);
+});
+
+modalButton.addEventListener('click', function (event) {
+    console.log(this);
+});
