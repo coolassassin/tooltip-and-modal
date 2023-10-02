5e61e82b65d87d4ddf98c9a91722069986ad4df0
JuliaKud <xovotat@gmail.com>
2023-10-02 18:04:12 +0200

    modal window

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import './style.css';
 
-document.querySelector(".button2").addEventListener("click", function() {
+document.querySelector(".button2").addEventListener("click", function () {
     helptip(this);
 });
 
 function helptip(t) {
     helptip.v = 0;
-    let b = t.children[(t.children.length-1)];
-    if (b.style.display==="block") {
+    let b = t.children[(t.children.length - 1)];
+    if (b.style.display === "block") {
         b.style.display = "none";
     } else {
         helptipx();
@@ -17,12 +17,28 @@ function helptip(t) {
 }
 
 function helptipx() {
-    if (helptip.v===1) {
+    if (helptip.v === 1) {
         helptip.v = 0;
         return;
     }
     let s = document.querySelectorAll(".helptip");
-    for (let i=0; i < s.length; i++) {
-        s[i].children[(s[i].children.length-1)].style.display = "none";
+    for (let i = 0; i < s.length; i++) {
+        s[i].children[(s[i].children.length - 1)].style.display = "none";
+    }
+}
+
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+span.onclick = function () {
+    modal.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
     }
 }
