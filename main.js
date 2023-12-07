b8adae2b50edd90b9433d8b0fc59f84bd4e4b210
Ekaterina Itsenko <ekaterinaicenko@gmail.com>
2023-12-07 15:24:05 +0100

    Add solution

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 import './style.css';
 
-console.log('Hello world');
+const buttonClick = document.getElementById('button-click');
+const buttonHover = document.getElementById("button-hover");
+const buttonModal = document.getElementById("button-modal");
+
+const tooltipClick = document.getElementById('tooltip-click');
+const tooltipHover = document.getElementById('tooltip-hover');
+const modal = document.getElementById("modal");
+const acceptButton = document.querySelector(".accept");
+const cancelButton = document.querySelector(".decline");
+
+let isClicked = false;
+buttonClick.addEventListener('click', () => {
+    if (isClicked) {
+        tooltipClick.style.display = 'none';
+        isClicked = false;
+    } else {
+        tooltipClick.style.display = 'block'
+        isClicked = true;
+    }
+});
+buttonHover.addEventListener('mouseenter', () => tooltipHover.style.display = 'block');
+buttonHover.addEventListener('mouseleave', () => tooltipHover.style.display = 'none');
+buttonModal.addEventListener("click", () => modal.style.display = "flex");
+acceptButton.addEventListener("click", () => modal.style.display = "none");
+cancelButton.addEventListener("click", () => modal.style.display = "none");
